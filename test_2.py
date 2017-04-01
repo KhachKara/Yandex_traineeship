8d97d887f72ab4854cba4f8a3dfe853a112edd8c
KhachKara <khachkara@gmail.com>
2017-04-02 01:12:37 +0300

    02_test2 right working version extended

--- test_2.py
+++ test_2.py
@@ -20,11 +20,22 @@
 #      public List getList();
 #  }
 
+# def func(_list):
+#     result=[]
+#     for i in _list:
+#         if type(i) == list:
+#             list(map(result.append, func(i)))
+#         else:
+#             result.append(i)
+#     return result
+#
+# print(func(["a", ["b", "c", ["d"], "e"]]))
+
 def func(_list):
     result=[]
     for i in _list:
         if type(i) == list:
-            list(map(result.append, func(i)))
+            result.extend(func(i))
         else:
             result.append(i)
     return result
