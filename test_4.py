6a8f68784a671fa82e944edac3ca7108c4efff97
khachkara@gmail.com <khachkara@gmail.com>
2017-04-05 11:07:29 +0300

    04_05.04.2017 вариант Мгера

--- test_4.py
+++ test_4.py
@@ -1,49 +1,37 @@
-# Двоичное дерево поиска
-# В заданном двоичном дереве поиска с дубликатами найдите все наиболее часто встречающиеся элементы.
-# Считайте, что двоичное дерево поиска определено следующим образом:
-# Левое поддерево узла состоит только из узлов с ключами, меньшими либо равными ключу данного узла.
-# Правое поддерево узла состоит только из узлов с ключами, большими либо равными ключу данного узла.
-# Левое поддерево и правое поддерево также являются двоичными деревьями поиска.
-# Примечание: в случае, если наиболее часто встречающихся элементов более одного, их можно возвращать в любом порядке.
-# Например, для дерева
-#    1
-#     \
-#      2
-#     /
-#    2
-# результат [2]
-# Элемент двоичного дерева поиска на Java описывается вот таким классом:
-# public class Node {
-#     public final int val;
-#     public final Node left;
-#     public final Node right;
-#     Node(int x) { val = x; }
-# }
 import random
-
+__path = []
 class BinTree:
     def __init__(self, node_number):  # инициализируем значение вершины дерева
         self.node_number = node_number
         self.left = None  # необязательные параметры левый потомок
         self.right = None  # необязательные параметры правый потомок
 
-    def add_node(self, user_number):
-        res = []
-        if user_number >= self.node_number:
-            if self.right is not None:
-                self.right.add_node(user_number)
-                res.append(str(self.node_number) + str('<-->') + str(user_number))
-            else:
-                self.right = BinTree(user_number)
-                res.append(str(self.node_number) + str('<-->') + str(BinTree(user_number)))
+
+    def add_node(self, user_number, fl = [0]):
+        global __path
+        if fl[0] == 0:
+            __path = [self.node_number]
         else:
-            if self.left is not None:
-                self.left.add_node(user_number)
-                res.append(str(user_number) + str('<-->') + str(self.node_number))
+            __path.append(self.node_number)
+        fl[0] += 1
+        if user_number >= self.node_number:
+            if self.right is None:
+                self.right = BinTree(user_number)
+                __path.append(user_number)
             else:
+                self.right.add_node(user_number)
+        else:
+            if self.left is None:
                 self.left = BinTree(user_number)
-                res.append(str(BinTree(user_number)) + str('<-->') + str(self.node_number))
-        return res
+                __path.append(user_number)
+            else:
+                self.left.add_node(user_number)
+        fl[0] -= 1
+        if fl[0] == 0:
+            r = __path[:]
+            __path = []
+            return r
+        return __path
 
     def __repr__(self):
         return str(self.node_number)
@@ -51,9 +39,8 @@ class BinTree:
 
 tree = BinTree(15)
 m = []
-j = 0
-while j < 50:
-    m.append(random.randint(0, 50))
-    j += 1
+for j in range(10):
+    m.append(random.randint(0, 20))
+print(m)
 for i in m:
     print(tree.add_node(i))
