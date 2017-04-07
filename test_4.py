97e4b3aef35c39dd0b54adf28aec93bea99fbe29
khachkara@gmail.com <khachkara@gmail.com>
2017-04-07 10:13:00 +0300

    04_06.04.2017 вариант Мгера + мои добавления

--- test_4.py
+++ test_4.py
@@ -4,4 +4,16 @@ class BinTree:
         self.node = node
         self.level = level
 
+class BinNode:
+    def __init__(self, left_son, right_son, node_name, node_key):
+        self.parent = None
+        self.left_son = left_son
+        self.right_son = right_son
+        self.node_name = node_name
+        self.node_key = node_key
+    def add_node(self):
+
+
+tree = BinTree(10, 20, 5)
+noode = BinNode(True, False, 'ABC', 15)
 
