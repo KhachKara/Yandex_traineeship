d8ac08595464b4951738eee29e2ef1ce871fa8bc
khachkara@gmail.com <khachkara@gmail.com>
2017-04-07 14:27:13 +0300

    04_07.04.2017 binary_sorting

--- test_4.py
+++ test_4.py
@@ -1,23 +1,5 @@
-class BinTree:
-    def __init__(self, node_name, node_key):
-        self.level = None
-        self.parent = None
-        self.left_son = None
-        self.right_son = None
-        self.node_name = node_name
-        self.node_key = node_key
-
-    def add_node(self, user_node, nodes = []):
-        if user_node > self.node_key:
-            self.right_son = self.node_key
-            nodes.append(self.node_key)
-        elif user_node < self.node_key:
-            self.left_son = self.node_key
-            nodes.append(self.node_key)
-        else:
-            return -1
-        return nodes
-
-tree = BinTree('ABC', 15)
-tree.add_node(10)
-print(tree.add_node())
+class node:
+    def __init__(self, data = None, left = None, right = None):
+        self.data   = data
+        self.left   = left
+        self.right  = right
