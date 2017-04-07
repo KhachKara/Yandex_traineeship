10d2bd64afdebb07fbe89134839bf5b6a75e3097
khachkara@gmail.com <khachkara@gmail.com>
2017-04-07 14:23:01 +0300

    04_07.04.2017 мой вариант

--- test_4.py
+++ test_4.py
@@ -1,19 +1,23 @@
 class BinTree:
-    def __init__(self, top, node, level):
-        self.top = top
-        self.node = node
-        self.level = level
-
-class BinNode:
-    def __init__(self, left_son, right_son, node_name, node_key):
+    def __init__(self, node_name, node_key):
+        self.level = None
         self.parent = None
-        self.left_son = left_son
-        self.right_son = right_son
+        self.left_son = None
+        self.right_son = None
         self.node_name = node_name
         self.node_key = node_key
-    def add_node(self):
 
+    def add_node(self, user_node, nodes = []):
+        if user_node > self.node_key:
+            self.right_son = self.node_key
+            nodes.append(self.node_key)
+        elif user_node < self.node_key:
+            self.left_son = self.node_key
+            nodes.append(self.node_key)
+        else:
+            return -1
+        return nodes
 
-tree = BinTree(10, 20, 5)
-noode = BinNode(True, False, 'ABC', 15)
-
+tree = BinTree('ABC', 15)
+tree.add_node(10)
+print(tree.add_node())
