d419951149cb3a191f87ecacd64a2e474636a14b
khachkara@gmail.com <khachkara@gmail.com>
2017-04-07 19:09:54 +0300

    04-1_07.04.2017 реализация через библиотеку

--- test_4-1.py
+++ test_4-1.py
@@ -3,34 +3,36 @@ import string
 
 
 class BinTree:
-    def __init__(self, node_name, node_key):
+    def __init__(self, node):
         self.level = None
         self.parent = None
         self.left_son = None
         self.right_son = None
-        self.node_name = node_name
-        self.node_key = node_key
+        self.node = node
 
     def add_node(self, user_node, nodes = []):
-        if user_node[1] > self.node_key:
-            self.right_son = user_node[1]
-            nodes.append(user_node[1])
-        elif user_node[1] < self.node_key:
-            self.left_son = user_node[1]
-            nodes.append(user_node[1])
+        if user_node[0] > dict.values(self.node):
+            self.right_son = user_node[0]
+            nodes.append(user_node[0])
+        elif user_node[0] < dict.values(self.node):
+            self.left_son = user_node[0]
+            nodes.append(user_node[0])
         else:
             return -1
         return nodes
 
 
-name = ['ABC']
+name = ['A']
 key = [15]
-tree = BinTree(name, key)
+Node = {'name': key}
+
+tree = BinTree(Node)
 
 for j in range(10):
     key.append(random.randint(0, 20))
-for k in string.ascii_uppercase:
-    name.append(random.randint(0, 26))
+for k in range(0, 26):
+    name.append(string.ascii_uppercase[random.randint(0, 25)])
+    if k == 10:
+        break
 tree.add_node([key], name)
 print(tree.add_node([key], name))
-
